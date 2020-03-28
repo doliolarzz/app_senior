849cfe08795acf9926ec14f4c122bf1ab1ae9bf2
Warit Prasartritha <doliolarzz@hotmail.com>
2020-03-29 02:05:31 +0700

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,37 +10,50 @@ import MomentUtils from '@date-io/moment';
 
 const theme = createMuiTheme({
   palette: {
-      primary: {
-          light: '#ffc947',
-          main: '#ff9800',
-          dark: '#c66900',
-          contrastText: '#fff',
-      },
-      secondary: {
-          light: '#6d6d6d',
-          main: '#424242',
-          dark: '#1b1b1b',
-          contrastText: '#fff',
-      },
+    primary: {
+      light: '#ffc947',
+      main: '#ff9800',
+      dark: '#c66900',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#6d6d6d',
+      main: '#424242',
+      dark: '#1b1b1b',
+      contrastText: '#fff',
+    },
   },
   typography: {
-      fontFamily: '"Kanit", sans-serif',
-      fontWeightLight: 200,
-      fontWeightRegular: 300,
-      fontWeightMedium: 400,
-      useNextVariants: true,
-      textPrimary: '#000000',
-      textSecondary: '#FFFFFF',
+    fontFamily: '"Kanit", sans-serif',
+    fontWeightLight: 200,
+    fontWeightRegular: 300,
+    fontWeightMedium: 400,
+    useNextVariants: true,
+    textPrimary: '#000000',
+    textSecondary: '#FFFFFF',
   },
+  overrides: {
+    MuiSlider: {
+      thumb: {
+        color: 'white',
+      },
+      track: {
+        color: 'white',
+      },
+      rail: {
+        color: 'white',
+      }
+    }
+  }
 });
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
-      <MuiPickersUtilsProvider utils={MomentUtils}>
-          {/* <Provider store={store}> */}
-              <App />
-          {/* </Provider> */}
-      </MuiPickersUtilsProvider>
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      {/* <Provider store={store}> */}
+      <App />
+      {/* </Provider> */}
+    </MuiPickersUtilsProvider>
   </MuiThemeProvider>,
   document.getElementById('root')
 );
